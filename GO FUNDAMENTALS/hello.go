295e0979da00d0419bbc0da570f61a166a2fbb43
Ronald Fucks <ronaldfuchs@gmail.com>
2023-06-10 20:02:42 -0300

    hello.go update with 2 tests

--- GO FUNDAMENTALS/hello.go
+++ GO FUNDAMENTALS/hello.go
@@ -5,9 +5,12 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("man"))
+	fmt.Println(Hello(""))
 }
